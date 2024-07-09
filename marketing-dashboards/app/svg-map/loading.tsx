1459155822d195fb071880f4523ab0ac5229377c
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2024-07-09 13:25:16 +0100

    create non-data loading skeleton

---
+++ marketing-dashboards/app/svg-map/loading.tsx
@@ -0,0 +1,6 @@
+import ChoroplethLoading from "@/Components/Maps/DynamicSVG/ChoroplethLoading";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return <ChoroplethLoading />
+  }
