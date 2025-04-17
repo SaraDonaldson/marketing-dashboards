201fecf373da47a071088fc6614f76b4a37b9c6e
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2025-04-17 16:09:58 +0100

    Update menu items

--- marketing-dashboards/Components/Navigation/DrawerMenu/DrawerMenu.tsx
+++ marketing-dashboards/Components/Navigation/DrawerMenu/DrawerMenu.tsx
@@ -13,6 +13,7 @@ import { usePathname} from 'next/navigation'
 const DrawerMenu: React.FC = () => {
    const pathname = usePathname()
    
+   
 return (
     <div className="xs:hidden md:flex h-full bg-slate-900 z-30" >
     <div  className="xs:hidden md:flex h-screen md:ml-3 lg:ml-6 xl:ml-12 flex flex-col z-20 pl-6 py-4 bg-slate-900 w-full">
@@ -21,8 +22,43 @@ return (
       <span className="sticky pt-24 font-semibold uppercase text-slate-300 py-2">Menu</span>
   <div className="relative py-4 pr-16 w-full scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-slate-900 overflow-y-scroll">
       <div className="space-y-2 xs:text-xs lg:text-sm text-white">
+      
+      <div>
+            <Link href="/dashboard" className={`flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("dashboard") && `text-purple-500 bg-gray-700`}`}>
+            <VscGraphLine />
+               <span className="ms-3">Dashboards</span>
+            </Link>
+         </div>
+
+   {/* { pathname.includes("/dashboard") &&
+      <> */}
          <div>
-            <Link href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+            <Link href="/dashboard/adscampaign" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("adscampaign") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
+               <span className={`ms-3 `}>Google Ads Campaign</span>
+            </Link>
+         </div>
+         <div>
+            <Link href="/dashboard/emailconversion" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("emailconversion") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
+               <span className={`ms-3 `}>Email List</span>
+            </Link>
+         </div>
+         <div>
+            <Link href="/dashboard/salesconversion" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("salesconversion") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
+               <span className={`ms-3 `}>Sales Conversion Stats</span>
+            </Link>
+         </div>
+         <div>
+            <Link href="/dashboard/websitevisitors" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("websitevisitors") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
+               <span className={`ms-3 `}>Website Visitors</span>
+            </Link>
+         </div>
+         {/* </>
+   }
+         */}
+        
+        
+         <div>
+            <Link href="/geo" className={`flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("geo") && `text-purple-500 bg-gray-700`}`}>
             <FaMapMarkedAlt />
                <span className="ms-3">Maps and Geo</span>
             </Link>
@@ -30,7 +66,7 @@ return (
 
      
          <div>
-            <Link href="/svg-map" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("svg-map") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
+            <Link href="/geo/svg-map" className={`flex items-center ms-2 p-2 font-light hover:rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 group ${pathname.includes("svg-map") && "text-purple-500 border-l-[0.5px] border-purple-500"} `}>
                <span className={`ms-3 `}>SVG Choropleth Map</span>
             </Link>
          </div>
@@ -94,139 +130,7 @@ return (
             </a>
          </div>
       }
-
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-
-         <div>
-         <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 transition duration-75 group">
-         <PiSignOut/> 
-               <span className="ms-3">Sign Out</span>
-            </a>
-         </div>
-
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <VscGraphLine />
-               <span className="ms-3">Dashboard</span>
-            </a>
-         </div>
-
-         {pathname.includes("admin") &&
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <RiFolderSettingsLine />
-               <span className="ms-3">Manage Clients</span>
-            </a>
-         </div>
-      }
-
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-         <div>
-            <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <CiMoneyCheck1 />
-               <span className="ms-3">Transactions</span>
-            </a>
-         </div>
-
-
-         <div>
-         <a href="#" className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100 dark:hover:bg-gray-700 transition duration-75 group">
-         <PiSignOut/> 
-               <span className="ms-3">Sign Out</span>
-            </a>
-         </div>
-      
+   
         </div>
         </div>
     </div>
